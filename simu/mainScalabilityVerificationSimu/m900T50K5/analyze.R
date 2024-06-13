c33c259b75869b91352cad9a391de1a2419372ca
Yifan Cheng <51808665+anniecyf@users.noreply.github.com>
2024-06-13 17:59:56 +0800

    Add files via upload

---
+++ simu/mainScalabilityVerificationSimu/m900T50K5/analyze.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+setwd("C:/Users/annie/OneDrive - National University of Singapore/Documents/PhD/research/server/spatempBFA/simu/mainScalabilityVerificationSimu/m900T50K5")
+list.files()
+load("regFixedL50simuT50M900Iter30000.RData")
+load("regFixedL50simuBlockT50M900Iter30000.RData")
+load("regFixedL50simuSequenT50M900Iter30000nostorealphaweights.RData")
+load("regVaryLjsimuSequenT50M900Iter30000nostorealphaweight.RData")
+GibbsStepTimeFixedLfullGP <- regFixedL.simu$GibbsStepTime
+GibbsStepTimeFixedLblock <- regFixedL.simu.block$GibbsStepTime
+GibbsStepTimeFixedLsequen <- regFixedL.simu.sequen$GibbsStepTime
+GibbsStepTimeVaryLjSequen <- regVaryLj.simu.sequen$GibbsStepTime
+save(GibbsStepTimeFixedLfullGP, file = "GibbsStepTimeFixedLfullGP.RData")
+save(GibbsStepTimeFixedLblock, file = "GibbsStepTimeFixedLblock.RData")
+save(GibbsStepTimeFixedLsequen, file = "GibbsStepTimeFixedLsequen.RData")
+save(GibbsStepTimeVaryLjSequen, file = "GibbsStepTimeVaryLjSequen.RData")
+
+load("GibbsStepTimeFixedLfullGP.RData")
+load("GibbsStepTimeFixedLblock.RData")
+load("GibbsStepTimeFixedLsequen.RData")
+load("GibbsStepTimeVaryLjSequen.RData")
+
+
+
+
+
+
+
+
+
+
+
